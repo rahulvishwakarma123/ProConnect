ab689e4b55efeea4b6d8d4ecb275f34955fa9f6e
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-08-25 11:38:57 +0530

    Updated the backend URL

--- Frontend/src/config/index.jsx
+++ Frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL =  'http://localhost:9080'
+export const BASE_URL =  'https://proconnect-9m19.onrender.com/'
 
 export const clientServer = axios.create({
     baseURL: BASE_URL
